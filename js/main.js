015eaddddc058e052a57e10cfedf4b2d0243eec4
Faith Wallace <fwallacephd@gmail.com>
2018-04-21 16:18:31 -0400

    added functionality to first question

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Global Variables
+var score = 0;
+
 //Hide Questions
 $("#question1").hide();
 
@@ -7,3 +10,13 @@ $(".take-quiz").on("click", function(){
   $("#question1").show();
 });
 
+//Question 1
+$(".question1-submit").on("click". function(){
+  var answer1 = $("[name=episode]:checked").val();
+  if (answer1 === "Astronaut") {
+    score += 1;
+    alert("Correct! You have " + score + " point(s) out of 5!")
+  } else {
+    alert("Oops! That was incorrect. You have " + score + " point(s) out of 5!")
+  }
+});
